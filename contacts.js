72550aafd45ac8045fd3ed30d4365d72ceb4d6ba
GolovinaElia <eliagolovina@ukr.net>
2021-06-29 17:51:10 +0300

    Добавляет файлы

---
+++ contacts.js
@@ -0,0 +1,22 @@
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  console.log('list');
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
